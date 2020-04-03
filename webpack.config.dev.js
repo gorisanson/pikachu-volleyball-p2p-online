15666ec66ee797e718f330faa34db8097fd4d308
Lee, Kyutae <gorisanson@gmail.com>
2020-04-04 00:08:26 +0900

    Update webpack.config.dev.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,27 +9,47 @@ module.exports = {
     contentBase: './dist'
   },
   entry: {
-    main: './src/resources/js/main_online.js'
+    main: './src/resources/js/main_online.js',
+    ko: './src/ko/ko.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   optimization: {
-    runtimeChunk: { name: 'runtime' } // this is for code-sharing between "main.js and "ko.js"
+    runtimeChunk: { name: 'runtime' }, // this is for code-sharing between "main.js and "ko.js"
+    splitChunks: {
+      chunks: 'all'
+    }
   },
   plugins: [
     new CopyPlugin([
-      { from: 'src/resources/assets/', to: 'resources/assets/' },
+      {
+        context: 'src/',
+        from: 'resources/assets/**/*.+(json|png|mp3|wav)'
+      },
       { from: 'src/resources/style.css', to: 'resources/style.css' },
       { from: 'src/index.html', to: 'index.html' }
     ]),
     new HtmlWebpackPlugin({
       template: 'src/en/index.html',
       filename: 'en/index.html',
-      hash: true,
       chunks: ['runtime', 'main'],
-      chunksSortMode: 'manual'
+      chunksSortMode: 'manual',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/ko/index.html',
+      filename: 'ko/index.html',
+      chunks: ['runtime', 'ko', 'main'],
+      chunksSortMode: 'manual',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true
+      }
     })
   ]
 };
