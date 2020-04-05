5ec637e4bec3174f958880d9b31736639a388f73
Lee, Kyutae <gorisanson@gmail.com>
2020-04-06 00:21:35 +0900

    Reflect from offline version

--- src/resources/js/offline_version_js/rand.js
+++ src/resources/js/offline_version_js/rand.js
@@ -1,7 +1,11 @@
+/**
+ * This module contains random number generator used for the game
+ */
 'use strict';
-export const forRand = {
-  rng: null
-};
+/** @typedef {function():number} RNG */
+
+/** @type {RNG} custom RNG (random number generator) function which generates a random number in [0, 1] */
+let customRng = null;
 
 /**
  * Return random integer in [0, 32767]
@@ -12,9 +16,22 @@ export const forRand = {
  * I decided to use custom rand function which generates random integer in [0, 32767]
  * which follows rand() function in Visual Studio 2017 Library.
  *
+ * By default, it uses the function "Math.random" for generating a random number.
+ * A custom RNG function can used by setting the "customRng" as the custom RNG function.
+ *
  * @return {number} random integer
  */
 export function rand() {
-  return Math.floor(32768 * forRand.rng());
-  // return Math.floor(32768 * Math.random());
+  if (customRng === null) {
+    return Math.floor(32768 * Math.random());
+  }
+  return Math.floor(32768 * customRng());
+}
+
+/**
+ * Set custom RNG function
+ * @param {RNG} rng
+ */
+export function setCustomRng(rng) {
+  customRng = rng;
 }
