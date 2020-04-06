0ad14af46dcf2d100068c3d1c3f97d8f04f7c457
Lee, Kyutae <gorisanson@gmail.com>
2020-04-06 23:15:35 +0900

    Refactor webpack config files by seperating ...
    common configuration.

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/resources/js/main_online.js',
+    ko: './src/ko/ko.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: { name: 'runtime' }, // this is for code-sharing between "main.js and "ko.js"
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {
+        context: 'src/',
+        from: 'resources/assets/**/*.+(json|png|mp3|wav)',
+      },
+      { from: 'src/resources/style.css', to: 'resources/style.css' },
+      { from: 'src/index.html', to: 'index.html' },
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'src/en/index.html',
+      filename: 'en/index.html',
+      chunks: ['runtime', 'main'],
+      chunksSortMode: 'manual',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/ko/index.html',
+      filename: 'ko/index.html',
+      chunks: ['runtime', 'ko', 'main'],
+      chunksSortMode: 'manual',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+};
