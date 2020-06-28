8fc6a445181e42df012f8da43b98e035c030a198
samslow <shsvkdldj@likelion.org>
2020-06-28 17:32:07 +0900

    Adjust classlist dynamical

--- src/resources/js/ui_online.js
+++ src/resources/js/ui_online.js
@@ -344,7 +344,7 @@ export function setUpUI() {
     const roomId = generatePushID();
     createRoom(roomId).then(() => {
       printCurrentRoomID(roomId);
-      copyBtn.style.visibility = 'visible';
+      copyBtn.classList.remove('hide');
     });
   });
 
