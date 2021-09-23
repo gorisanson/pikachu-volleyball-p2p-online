946eba1689740b525a4ac485996adb858011a426
Lee, Kyutae <gorisanson@gmail.com>
2021-09-23 17:34:53 +0900

    Create alert box to be shown if embedded
    in other website and make it work.

--- src/resources/js/main_online.js
+++ src/resources/js/main_online.js
@@ -79,7 +79,28 @@ channel.callbackAfterDataChannelOpened = () => {
   loader.load(setup);
 };
 
+setUpUIForIfEmbeddedInOtherWebsite();
 setUpUI();
+
+function setUpUIForIfEmbeddedInOtherWebsite() {
+  /**
+   * Check if the page is embedded in other site.
+   * Copied from: https://stackoverflow.com/a/326076/8581025
+   */
+  const isEmbeddedInOtherWebsite = () => {
+    try {
+      return window.self !== window.top;
+    } catch (e) {
+      return true;
+    }
+  };
+
+  if (isEmbeddedInOtherWebsite()) {
+    Array.from(
+      document.getElementsByClassName('if-embedded-in-other-website')
+    ).forEach((elem) => elem.classList.remove('hidden'));
+  }
+}
 
 /**
  * Set up the loader progress bar.
