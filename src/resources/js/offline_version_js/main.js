c564400888c74912a7724332979a13083a240baa
Lee, Kyutae <gorisanson@gmail.com>
2021-12-18 08:27:33 +0900

    Copy offline version js

--- src/resources/js/offline_version_js/main.js
+++ src/resources/js/offline_version_js/main.js
@@ -89,12 +89,18 @@ function setUpInitialUI() {
     if (!aboutBox.classList.contains('hidden')) {
       aboutBox.classList.add('hidden');
       // @ts-ignore
-      gameDropdownBtn.disabled = false;
-      // @ts-ignore
-      optionsDropdownBtn.disabled = false;
+      aboutBtn.disabled = true;
     }
     aboutBtn.getElementsByClassName('text-play')[0].classList.add('hidden');
     aboutBtn.getElementsByClassName('text-about')[0].classList.remove('hidden');
+    aboutBtn.classList.remove('glow');
+    closeAboutBtn
+      .getElementsByClassName('text-play')[0]
+      .classList.add('hidden');
+    closeAboutBtn
+      .getElementsByClassName('text-close')[0]
+      .classList.remove('hidden');
+    closeAboutBtn.classList.remove('glow');
 
     loader.load(setup); // setup is called after loader finishes loading
     loadingBox.classList.remove('hidden');
@@ -103,6 +109,36 @@ function setUpInitialUI() {
   };
   aboutBtn.addEventListener('click', closeAboutBox);
   closeAboutBtn.addEventListener('click', closeAboutBox);
+
+  /**
+   * Check if the page is embedded in other site.
+   * Copied from: https://stackoverflow.com/a/326076/8581025
+   */
+  const isEmbeddedInOtherWebsite = () => {
+    try {
+      return window.self !== window.top;
+    } catch (e) {
+      return true;
+    }
+  };
+
+  if (isEmbeddedInOtherWebsite()) {
+    document
+      .getElementById('flex-container')
+      .classList.add('embedded-in-other-website');
+    Array.from(
+      document.getElementsByClassName('if-embedded-in-other-website')
+    ).forEach((elem) => elem.classList.remove('hidden'));
+    Array.from(
+      document.querySelectorAll('.if-embedded-in-other-website button')
+    ).forEach((elem) =>
+      elem.addEventListener('click', () => {
+        Array.from(
+          document.getElementsByClassName('if-embedded-in-other-website')
+        ).forEach((elem) => elem.classList.add('hidden'));
+      })
+    );
+  }
 }
 
 /**
