89d4b0c368e8f006a7a578e72c6543b86c3c0046
Lee, Kyutae <gorisanson@gmail.com>
2022-01-17 20:40:41 +0900

    Remove logic for detecting if embedded
    in other website from main_online.js

--- src/resources/js/main_online.js
+++ src/resources/js/main_online.js
@@ -80,38 +80,8 @@ channel.callbackAfterDataChannelOpened = () => {
   loader.load(setup);
 };
 
-setUpUIForIfEmbeddedInOtherWebsite();
 setUpUI();
 setUpUIForBlockingOtherUsers();
-
-function setUpUIForIfEmbeddedInOtherWebsite() {
-  /**
-   * Check if the page is embedded in other site.
-   * Copied from: https://stackoverflow.com/a/326076/8581025
-   */
-  const isEmbeddedInOtherWebsite = () => {
-    try {
-      return window.self !== window.top;
-    } catch (e) {
-      return true;
-    }
-  };
-
-  if (isEmbeddedInOtherWebsite()) {
-    Array.from(
-      document.getElementsByClassName('if-embedded-in-other-website')
-    ).forEach((elem) => elem.classList.remove('hidden'));
-    Array.from(
-      document.querySelectorAll('.if-embedded-in-other-website button')
-    ).forEach((elem) =>
-      elem.addEventListener('click', () => {
-        Array.from(
-          document.getElementsByClassName('if-embedded-in-other-website')
-        ).forEach((elem) => elem.classList.add('hidden'));
-      })
-    );
-  }
-}
 
 /**
  * Set up the loader progress bar.
