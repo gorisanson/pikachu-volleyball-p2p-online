718602d5c94df3833bc9814c626af8aa828c0b87
Kyutae Lee <gorisanson@gmail.com>
2023-03-01 21:48:25 +0900

    Change cache option for fetch to default
    from no-cache.
    When I tested, Chromium did not cache the results of a preflight request
    if "no-cache" option was used. So change it to "default".

--- src/resources/js/quick_match/quick_match.js
+++ src/resources/js/quick_match/quick_match.js
@@ -104,7 +104,7 @@ async function postData(url = '', data = {}) {
     const response = await fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
       mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         'Content-Type': 'application/json',
